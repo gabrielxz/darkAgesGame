5cc9d1b728499fe5141bfd3dcd1e9de4c90c08cf
rbharris42 <rbharris42@gmail.com>
2014-03-01 17:46:51 -0500

    more audio
    more audio

--- web/darkages.dart
+++ web/darkages.dart
@@ -8,7 +8,9 @@ part 'menu.dart';
 part 'city.dart';
 
 ResourceManager resourceManager;
-var soundChannel;
+var soundChannel1;
+var soundChannel2;
+
 
 Sprite countryOneButton;
 TextField textField;
@@ -34,7 +36,7 @@ void main()
   ..addBitmapData('map', 'images/MapRender-Real-1280x720.jpg')
   ..addBitmapData('hammer', 'images/spaceHammer.jpg')
   ..addSound('ambientMusic', 'sounds/ambient.mp3')
-  ..addBitmapData('hammer', 'images/spaceHammer.jpg');
+  ..addSound('tensionMusic', 'sounds/moon_virus_Tension_master.wav');
   //..addBitmapData('button', 'images/button.jpg')
   //..addSound('backgroundMusic', 'images/JACKSTEP.mp3');
   
@@ -75,7 +77,7 @@ resourceManager.load().then((result)
   stage.addChild(testGraph);
   
   musicLoop();
-  
+  //new Timer(new Duration(seconds: 5), fadeTensionIn);
 
   city_init();
   renderCities();
@@ -91,9 +93,42 @@ resourceManager.load().then((result)
 void musicLoop(){
   print("musicLoop");
   var sound1 = resourceManager.getSound("ambientMusic");
-  var soundTransform1 = new SoundTransform(0.5);
-  soundChannel = sound1.play(false, soundTransform1);
-  new Timer(new Duration(seconds: 10), musicLoop);
+  var soundTransform0 = new SoundTransform(0.5);
+  soundChannel1 = sound1.play(false, soundTransform0);
+  new Timer(new Duration(seconds: 116), musicLoop);
+
+}
+
+
+void fadeTensionIn(){
+  print("fadeActionIn");
+  var sound2 = resourceManager.getSound("tensionMusic");
+  var soundTransform0 = new SoundTransform(0.1);
+  soundChannel2 = sound2.play(false, soundTransform0);
+  new Timer(new Duration(milliseconds: 500), fadeTensionLevel1);
+}
+
+void fadeTensionLevel1(){
+  print("fadeTensionLevel1");
+  soundChannel2.soundTransform = new SoundTransform(0.2);
+  new Timer(new Duration(milliseconds: 500), fadeTensionLevel2);
+}
+
+void fadeTensionLevel2(){
+  print("fadeTensionLevel2");
+  soundChannel2.soundTransform = new SoundTransform(0.3);
+  new Timer(new Duration(milliseconds: 500), fadeTensionLevel3);
+}
+
+void fadeTensionLevel3(){
+  print("fadeTensionLevel3");
+  soundChannel2.soundTransform = new SoundTransform(0.4);
+  new Timer(new Duration(milliseconds: 500), fadeTensionLevel4);
+}
+
+void fadeTensionLevel4(){
+  print("fadeTensionLevel4");
+  soundChannel2.soundTransform = new SoundTransform(0.5);
 }
 
 renderCities()
